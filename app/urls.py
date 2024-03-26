728f0fbd279672dd037ada36796cfbe63beb0b95
lazyPy <janopatundog@gmail.com>
2024-03-26 21:46:40 +0800

    .

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('login', login_user, name='login'),
+
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name='forgot_password.html'), name='password_reset'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetView.as_view(template_name='password_reset_done.html'), name='password_reset_complete'),
+
     # path('register', register_user, name='register'),
     path('logout', logout_user, name='logout'),
     path('donate', donate_user, name='donate'),
